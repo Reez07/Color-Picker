a4987483d312ba906a81356629f72431b8e9c100
R Sreelekshmi <77920934+Reez07@users.noreply.github.com>
2021-08-17 18:48:56 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+function makeTeal()
+{
+    document.body.style.backgroundColor="teal";
+}
+
+function makeOrchid()
+{
+    document.body.style.backgroundColor="orchid";
+}
+
+function makeLightgreen()
+{
+    document.body.style.backgroundColor="lightgreen";
+}
+
+function makeOrange()
+{
+    document.body.style.backgroundColor="rgb(241, 113, 113)";
+}
